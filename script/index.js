a0b21a692dfb66a58d6f73e66110f54900cd8350
gabdykalyk <gabdykalyk@gmail.com>
2020-07-22 20:56:52 +0600

    Clear btn fixed

--- script/index.js
+++ script/index.js
@@ -23,7 +23,7 @@ function renderTask(task) {
                     </form>
                 </div>
                 <div class="tasks__delete">
-                    <a href="">
+                    <a href="" data-action="remove">
                         <svg class="delete" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
